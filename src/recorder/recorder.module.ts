893593a9bb29ab11913796e8647b169f18d1c2a6
hyeonwoojo <h_jo@ejn.gg>
2023-10-03 16:25:38 +0900

    fix: dependency

--- src/recorder/recorder.module.ts
+++ src/recorder/recorder.module.ts
@@ -4,9 +4,13 @@ import { ParserModule } from 'src/parser/parser.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Segment } from './entities/segment.entity';
 import { Stream } from './entities/stream.entity';
+import { Streamer } from './entities/streamer.entity';
 
 @Module({
-  imports: [ParserModule, SequelizeModule.forFeature([Segment, Stream])],
+  imports: [
+    ParserModule,
+    SequelizeModule.forFeature([Segment, Stream, Streamer]),
+  ],
   providers: [RecorderService],
 })
 export class RecorderModule {}
