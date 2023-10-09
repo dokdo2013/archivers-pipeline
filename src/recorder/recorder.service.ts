06bccb105e8f4b963bd45a525b342312056a2c2d
hyeonwoojo <h_jo@ejn.gg>
2023-10-09 11:05:50 +0900

    feat: set thumbnail try count to 50 times

--- src/recorder/recorder.service.ts
+++ src/recorder/recorder.service.ts
@@ -91,8 +91,8 @@ export class RecorderService {
         // 6. sleep for 2 seconds (using promise)
         await new Promise((resolve) => setTimeout(resolve, 2000));
 
-        // 7. when the segment count is 20, then save thumbnail image (when fail, retry 10 times)
-        if (segmentCount % 20 === 0 && segmentCount <= 200) {
+        // 7. when the segment count is 20, then save thumbnail image (when fail, retry 50 times)
+        if (segmentCount % 20 === 0 && segmentCount <= 1000) {
           const thumbnailKey = `archivers:recorder:${streamId}:thumbnail`;
           const hasThumbnail = await this.redisClient.get(thumbnailKey);
 
